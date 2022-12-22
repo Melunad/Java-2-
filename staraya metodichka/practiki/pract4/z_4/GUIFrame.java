e2c6ccd3ec83750a01afedbc5046d9b8ffae4d12
Melunad <121242728+Melunad@users.noreply.github.com>
2022-12-22 19:58:52 +0300

    Add files via upload

---
+++ staraya metodichka/practiki/pract4/z_4/GUIFrame.java
@@ -0,0 +1,57 @@
+package z_4;
+
+import javax.swing.*;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIFrame extends JFrame{
+    private JPanel Panel;
+    private JButton button1;
+    private JButton butt0n2;
+    private JLabel res;
+    private JLabel last;
+    private JLabel win;
+    int x1 = 0,x2 = 0;
+
+    public GUIFrame()  {
+        setContentPane(Panel);
+        setVisible(true);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        button1.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                x1++;
+                res.setText("Result: " + x1 +" x " + x2);
+                if(x1>x2)
+                    win.setText("Milan");
+                else if(x1 == x2)
+                    win.setText("DRAW");
+                last.setText("Milan");
+
+
+            }
+
+        });
+        butt0n2.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                x2++;
+                res.setText("Result: " + x1 +" x " + x2);
+                if(x1<x2)
+                    win.setText("Madrid");
+                else if(x1 == x2)
+                    win.setText("DRAW");
+                last.setText("Madrid");
+            }
+        });
+    }
+    public static void main(String[] args) {
+        new GUIFrame();
+
+    }
+
+
+}
